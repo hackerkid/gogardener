df5777d3247db53e8df305c13c390663abf71405
Vishnu KS <yo@vishnuks.com>
2020-08-08 15:46:37 +0530

    Create index.html file.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,8 @@
 * Make backlinks work
+    * Fetch the links the in the page
+    * Store them in backlink dict
+
+* Don't delete the files in .git folder
 * Make tags work
 * Make images work.
 * Figure out whether to store the generated files in the same repo as markdown files
@@ -10,3 +14,8 @@
 * ~Convert only .md files~
 * ~Take an ignore tag attribute~
 * ~Paste the generated content in base.html~
+* Make index.html work
+    * Go through the filenames
+    * Get the HTML path from file name
+    * Create unordered list of links
+    * Paste them inside baseHtml
--- gardener.go
+++ gardener.go
@@ -10,7 +10,17 @@ import (
 	"strings"
 	"path/filepath"
 	"flag"
+	//"regexp"
 )
+
+func getMDFileNameWithoutExtension(fileName string) string {
+	return strings.TrimSuffix(fileName, ".md")
+}
+
+func getHTMLFilePathFromFileName(fileName string) string {
+	var mdFileNameWithoutExtension = getMDFileNameWithoutExtension(fileName)
+	return strings.ToLower(strings.ReplaceAll(mdFileNameWithoutExtension, " ", "-")) + ".html"
+}
 
 func main() {
 
@@ -43,12 +53,12 @@ func main() {
 
 	processedFileCount := 0
 	fileNameToContentMap := make(map[string][]byte)
+	//backLinksMap := make(map[string][]string)
 
 	for _, f := range files {
 		fileName := f.Name()
 		input_file_extension := filepath.Ext(fileName)
 		if (input_file_extension != ".md") {
-			fmt.Println("Skipping", fileName)
 			continue
 		}
 
@@ -73,25 +83,27 @@ func main() {
 			continue
 		}
 
+		//re := regexp.MustCompile(`\[.+\]`)
+		//fmt.Printf("%q\n", re.FindAll([]byte(content), -1))
+
 		fileNameToContentMap[fileName] = b
 
+	}
+	
+	baseTemplateBytes, err := ioutil.ReadFile(baseTemplatePath)
+	if (err != nil) {
+		log.Fatal(err)
 	}
 
 	for fileName, byteContent := range fileNameToContentMap {
 		byteOutput := markdown.ToHTML(byteContent, nil, nil)
 		output := string(byteOutput)
 
-		mdFileNameWithoutExtension := strings.TrimSuffix(fileName, ".md")
-		if (len(baseTemplatePath) != 0) {
-			baseTemplateBytes, err := ioutil.ReadFile(baseTemplatePath)
-			if (err != nil) {
-				log.Fatal(err)
-			}
+		mdFileNameWithoutExtension := getMDFileNameWithoutExtension(fileName)
 
-			baseTemplate := string(baseTemplateBytes)
-			output = strings.ReplaceAll(baseTemplate, "{{ content }}", output)
-			output = strings.ReplaceAll(output, "{{ title }}", mdFileNameWithoutExtension)
-		}
+		baseTemplate := string(baseTemplateBytes)
+		output = strings.ReplaceAll(baseTemplate, "{{ content }}", output)
+		output = strings.ReplaceAll(output, "{{ title }}", mdFileNameWithoutExtension)
 
 		htmlFileName := strings.ToLower(strings.ReplaceAll(mdFileNameWithoutExtension, " ", "-")) + ".html"
 		ouput_filepath := path.Join(outputDir, htmlFileName)
@@ -102,5 +114,22 @@ func main() {
 		}
 		processedFileCount += 1
 	}
-	fmt.Println(processedFileCount,"/", len(files),  "files 📁 processed")
+
+	listOfPages := "<ul>\n"
+	for fileName := range fileNameToContentMap {
+		htmlFilePath := getHTMLFilePathFromFileName(fileName)
+		mdFileNameWithoutExtension := getMDFileNameWithoutExtension(fileName)
+		row := fmt.Sprintf("<li><a href=\"%s\">%s</a></li>\n", htmlFilePath, mdFileNameWithoutExtension)
+		listOfPages += row
+	}
+	listOfPages += "\n</ul>"
+
+	baseTemplate := string(baseTemplateBytes)
+	output := strings.ReplaceAll(baseTemplate, "{{ content }}", listOfPages)
+	output = strings.ReplaceAll(output, "{{ title }}", "Home")
+	ouput_filepath := path.Join(outputDir, "index.html")
+	err = ioutil.WriteFile(ouput_filepath, []byte(output), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
